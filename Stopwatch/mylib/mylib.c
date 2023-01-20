e6ad6532ae778ab4c8d1525fffa33cfbde5cd89d
TaigahG <114576796+TaigahG@users.noreply.github.com>
2023-01-20 13:39:04 +0700

    Update mylib.c

--- Stopwatch/mylib/mylib.c
+++ Stopwatch/mylib/mylib.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 void states(int s0, int s1, int m, int b, int* S0, int *S1){
-    int a = s0 & !s1;
-    int h = s0 & !m & !b;
-    int c = (!s0) & s1 & m & !b;
+    int a = !s0 & s1 & m & !b;
+    int h = s0 & !s1 & !m & !b;
+    int c = s0 & !s1 & m & b;
     *S0 = a | h | c;
-    int d = s1 & !m & !b;
-    int e = !s1 & !m & b;
-    int f = !s0 & !s1 & m;
-    int g = s0 & s1 & m & b;
-    *S1 = d | e | f | g;
+    int d = !s1 & m & !b;
+    int e = !s0 & !s1 & b;
+    int f = !s0 & s1 & !m & !b;
+    *S1 = d | e | f;
 
 }
